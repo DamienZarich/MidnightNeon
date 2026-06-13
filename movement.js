eb40333d8137762cf22d08faabb3713d17ca4dd6
Damien Zarich <damienzarich@gmail.com>
2026-06-13 10:28:58 -0700

    linked all objects under the parent spaceship-container and fixed the js so all keys will eventually move

--- movement.js
+++ movement.js
@@ -1,15 +1,21 @@
-const Spaceship = document.querySelector('.Spaceship')
-let postionBottom = 50;
-let postionLeft = 10;
+document.addEventListener("DOMContentLoaded", () => {
+const spaceShip = document.querySelector(".Spaceship-Container");
+let positionBottom = 50;
+let positionLeft = 10;
 const keys = {
     w: false,
     a: false,
     d: false,
     s: false
 }
+window.addEventListener('click', () => {
+    positionBottom += 20
+ spaceShip.style.bottom = positionBottom + 'px'
+});
 window.addEventListener('keydown', (event) => {
     const pressedKey = event.key.toLowerCase();
 if (pressedKey in keys ) {
  keys[pressedKey] = false;
 }
+});
 });
