3a984bbba2d9c586bbc0c16a9c406a49a52b3565
Damien Zarich <damienzarich@gmail.com>
2026-06-14 12:32:11 -0700

    remade it so that it works for all movements and added moving down as  well as borders so you cant go off screen

--- movement.js
+++ movement.js
@@ -1,21 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-const spaceShip = document.querySelector(".Spaceship-Container");
-let positionBottom = 50;
-let positionLeft = 10;
-const keys = {
+  const spaceShip = document.querySelector(".Spaceship-Container");
+  let positionBottom = 50;
+  let positionLeft = 10;
+  
+  const keys = {
     w: false,
     a: false,
     d: false,
     s: false
-}
-window.addEventListener('click', () => {
-    positionBottom += 20
- spaceShip.style.bottom = positionBottom + 'px'
-});
-window.addEventListener('keydown', (event) => {
-    const pressedKey = event.key.toLowerCase();
-if (pressedKey in keys ) {
- keys[pressedKey] = false;
-}
-});
+  };
+
+  spaceShip.style.bottom = positionBottom + 'px';
+  window.addEventListener('keydown', (event) => {
+    const key = event.key.toLowerCase();
+    if (key === 'w') {
+      positionBottom += 20;
+      spaceShip.style.bottom = positionBottom + 'px';
+    }
+    else if (key === 's') {
+      positionBottom -= 20;
+      if (positionBottom < 0) {
+        positionBottom = 0;
+      }
+      spaceShip.style.bottom = positionBottom + 'px';
+    }
+  });
 });
